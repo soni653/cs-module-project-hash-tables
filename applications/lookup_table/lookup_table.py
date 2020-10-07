e2dd926bd84742c6686be0cfaa9d3f8a7004453e
soni653 <dave.family653@outlook.com>
2020-10-06 22:25:52 -0400

    Updated slowfun() to use  a dictionary instead of nested list

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,11 @@
 # Your code here
+import random
+import math
+
+# n = 15
+# m = 15
+# lookup = [[None] * m] * n
+lookup = {}
 
 
 def slowfun_too_slow(x, y):
@@ -15,6 +22,19 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    # if lookup[x][y] is not None:
+    #     return lookup[x][y]
+    if (x, y) in lookup:
+        return lookup[(x, y)]
+
+    v = math.pow(x, y)
+    v = math.factorial(v)
+    v //= (x + y)
+    v %= 982451653
+    #lookup[x][y] = v
+    lookup[(x, y)] = v
+
+    return v
 
 
 
